1ed88436875595e8aee58c2811f4e6359a5a2ab0
Sven Anders Robbestad <1560743+robbestad@users.noreply.github.com>
2023-09-08 11:14:34 +0200

    meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
+
+
+<svelte:head>
+  <title>SDrive video HLS upload microservice</title>
+  <meta name="title" content="SDrive video HLS upload microservice" />
+  <meta name="description" content="This is Proof of concept and is intended for demonstration purposes to show how to use SDrive to
+    upload and stream videos." />
+  <meta name="twitter:title" content="SDrive video HLS upload microservice" />
+  <meta name="twitter:description" content="  This is Proof of concept and is intended for demonstration purposes to show how to use SDrive to
+  upload and stream videos" />
+  <meta name="twitter:creator" content="@sdrive_app" />
+</svelte:head>
 <div class="main">
   <h1>SDrive video HLS upload microservice</h1>
   <p>
