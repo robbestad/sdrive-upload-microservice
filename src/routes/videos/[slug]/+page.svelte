be0320ff6e5a54e03aea52ce862db8cc733e451d
Sven Anders Robbestad <1560743+robbestad@users.noreply.github.com>
2023-09-08 09:28:32 +0200

    meta

--- src/routes/videos/[slug]/+page.svelte
+++ src/routes/videos/[slug]/+page.svelte
@@ -1,10 +1,49 @@
 <script>
   /** @type {import('./$types').PageData} */
   export let data;
+  let url = `https://sdrive.app/videos/{data.id}`;
+  let { id, title, description, image } = data;
+  let filesrc = data.playlist;
+  let ogType = "video.other";
+  let ogVideoTag = "viral";
+  let ogVideoType = "video/mp4";
 </script>
 
 <svelte:head>
-  <title>{data.id}</title>
+  <title>{id}</title>
+  <meta name="title" content={title} />
+  <meta name="description" content={description} />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+  <meta name="twitter:creator" content="@sdrive_app" />
+  <link rel="notImportant" href={url} />
+  <link rel="canonical" href={url} />
+  <title>{title}</title>
+  <meta content="#43B581" name="theme-color" />
+  <meta name="twitter:site" content="@sdrive_app" />
+  <meta name="twitter:image:src" content={image} />
+  <meta property="og:image" content={image} />
+  <meta property="og:title" content={title} />
+  <meta property="og:type" content={ogType} />
+  <meta property="og:site_name" content="Sdrive.app" />
+  <meta name="og:video:url" content={filesrc} />
+  <meta property="og:image:width" content="480" />
+  <meta property="og:image:height" content="360" />
+  <meta property="og:video:type" content={ogVideoType} />
+  <meta property="og:video:tag" content={ogVideoTag} />
+  <meta property="og:video:width" content="1280" />
+  <meta property="og:video:height" content="720" />
+  <meta name="twitter:player" content={url} />
+  <meta name="twitter:player:height" content="720" />
+  <meta name="twitter:player:width" content="1280" />
+  <meta name="twitter:player:stream" content={filesrc} />
+  <meta name="og:video:secure_url" content={filesrc} />
+  <meta name="twitter:card" content="player" />
+  <meta
+    name="twitter:player:stream"
+    content="https://sdrive-upload-microservice.vercel.app/video/{id}"
+  />
+  `;
 </svelte:head>
 
 <div class="main">
